3b8c5dd93294681003420bb3111f6ecc5d20d39b
Babitababy <kumaribabita2402@gmail.com>
2021-04-03 11:50:05 +0530

    MainActivity.java class is modified

--- app/src/main/java/com/example/endlessnavigationbar/MainActivity.java
+++ app/src/main/java/com/example/endlessnavigationbar/MainActivity.java
@@ -1,14 +1,102 @@
 package com.example.endlessnavigationbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.ArrayList;
+
+import javax.net.ssl.SNIHostName;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    RecylerViewAdapter recylerViewAdapter;
+    ArrayList<String> rowsArrayList = new ArrayList<>();
+
+    boolean isLoading = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        populateData();
+        initAdapter();
+        initScrollListener();
+    }
+
+    private void populateData()
+    {
+        int i=0;
+        for(i=0;i<20;i++)
+        {
+            rowsArrayList.add("item " + i);
+        }
+    }
+
+    private void initAdapter()
+    {
+        recylerViewAdapter = new RecylerViewAdapter(rowsArrayList);
+        recyclerView.setAdapter(recylerViewAdapter);
+    }
+
+    private void initScrollListener()
+    {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+
+                if (!isLoading) {
+                    if (linearLayoutManager != null && linearLayoutManager.findLastCompletelyVisibleItemPosition() == rowsArrayList.size() - 1) {
+                        //bottom of list!
+                        loadMore();
+                        isLoading = true;
+                    }
+                }
+            }
+
+
+                                         }
+        );
+    }
+    private void loadMore() {
+        rowsArrayList.add(null);
+        recylerViewAdapter.notifyItemInserted(rowsArrayList.size() - 1);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                rowsArrayList.remove(rowsArrayList.size() - 1);
+                int scrollPosition = rowsArrayList.size();
+               recylerViewAdapter.notifyItemRemoved(scrollPosition);
+                int currentSize = scrollPosition;
+                int nextLimit = currentSize + 10;
+
+                while (currentSize - 1 < nextLimit) {
+                    rowsArrayList.add("Item " + currentSize);
+                    currentSize++;
+                }
+
+               recylerViewAdapter.notifyDataSetChanged();
+                isLoading = false;
+            }
+        }, 2000);
+
+
     }
 }
